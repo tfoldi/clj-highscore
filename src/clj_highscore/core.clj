69d86372c29419a8c02c7477c41cb2d37f05244f
Tamas Foldi <tfoldi@starschema.net>
2015-08-28 15:27:36 +0000

    few changes

--- src/clj_highscore/core.clj
+++ src/clj_highscore/core.clj
@@ -9,19 +9,48 @@
             [compojure.handler :refer [site]]
             [compojure.core :refer [defroutes GET PUT POST DELETE ANY]]))
 
+;; You are not needed...
+#_ (defn- parse-number
+  "Reads a number from a string. Returns nil if not a number."
+  [s]
+  (if (re-find #"^-?\d+\.?\d*$" s)
+    (read-string s)))
+
+(defn- integerify
+  "Tries to convert a string to an integer or returns default-value/0 otherwise."
+ ; main arity
+ ([a-string default-value]
+  (try
+   (if (or (nil? a-string) (= "" a-string))
+       default-value
+       (Integer/valueOf a-string))
+   (catch Exception e default-value)))
+ ; helper arity for 0
+ ([a-string] (integerify a-string 0)))
+
 (defn all-scores
-  [game limit offset]
-  (db/query (env :database-url)
+  "Returns all scores for the game named game, starting from offset, max
+   limit results. If either offset or limit cannot be converted to an integer.
+   they will be set to 0 and 100 respectively."
+  [game offset limit]
+
+  (db/query (env :database-url "postgres:highscore")
             ["select *
             from scores w
             where game = ?
             order by score desc, gametime asc
-            limit ? offset ?" game (or limit "ALL") (or offset 0)]))
+            limit ? offset ?" game
+	    (integerify limit 100)
+	    (integerify offset)]))
 
-(defn- get-param
+(defn- POST-param
+  "Returns a POST parameter"
   [ctx param]
-  (get-in ctx [:request :form-params param])
-  )
+  (get-in ctx [:request :form-params param]))
+
+(defn- GET-param
+  "Returns a GET parameter"
+  [ctx param] (get-in ctx [:request :params param]))
 
 (defresource get-scores
              [game]
@@ -29,7 +58,7 @@
              :handle-ok (fn [ctx]
                           (generate-string
                             (vector game
-                                    (all-scores game (get-param ctx :offset) (get-param ctx :limit)))))
+                                    (all-scores game (GET-param ctx "offset") (GET-param ctx "limit")))))
              :available-media-types ["application/json"])
 
 (defresource add-score
@@ -41,7 +70,10 @@
              :post!
              (fn [context]
                (let [params (get-in context [:request :form-params])]
-                 (println "ADD")))
+        ;         (db/insert (env :database-url "postgres:highscore")
+         ;          :scores
+ 	
+))
              :handle-created (fn [_] (generate-string (all-scores "tableau-cnake")))
              :available-media-types ["application/json"])
 
